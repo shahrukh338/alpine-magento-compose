b5842d7f291842a50124924c925284f9b93b8c69
praneeth3722 <57389893+praneeth3722@users.noreply.github.com>
2021-07-23 18:04:12 +0530

    Update mydock.sh

--- mydock.sh
+++ mydock.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # This is a Docker installation!
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo su
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt update
-sudo apt install docker-ce
-sudo usermod -aG docker ${USER}
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt update
+apt install docker-ce
+usermod -aG docker ${USER}
